0c591dac356a420efdd5ecdf08edb93986245f35
Asif Hafeez <asif.hafeez@hotmail.co.uk>
2016-10-28 12:56:04 +0100

    added weather icon

--- public/js/weatherThermometer.js
+++ public/js/weatherThermometer.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
-  $.get("http://api.openweathermap.org/data/2.5/weather?q=london&units=metric&APPID=e61318947b3809669f6c6fef00eb1cac", function(weather) {
-  $('#weather').text(weather.main.temp);
-});
+	
+
+  $.get("http://api.openweathermap.org/data/2.5/weather?q=paris&units=metric&id=524901&APPID=60ca7c94fd1f029e9896819ad8876e76", function(weather) {
+	  var temperature = "Current temperature: " + weather.main.temp
+	  var name = weather.weather[0].description
+	  var thing = weather.weather[0].icon
+	  var iconPath = "http://openweathermap.org/img/w/" + thing + ".png"
+	  $('#weather').html("Description: " + name + " " + temperature );
+    $('<img src="'+ iconPath +'">').on('load', function() {
+ 			$(this).width(100).height(100).appendTo('#weather-icon');
+		});
+
+
+	});
 });
